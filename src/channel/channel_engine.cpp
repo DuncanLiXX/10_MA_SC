a195a5f06f6ccef5eb7cea2c13bcc805e72318fa
zoukun <zoukun@maxonic.com.cn>
2022-06-24 16:02:46 +0800

    解决子程序调用过程中 if endif 不匹配问题

--- src/channel/channel_engine.cpp
+++ src/channel/channel_engine.cpp
@@ -7365,6 +7365,7 @@ void ChannelEngine::CheckBattery(){
  */
 void ChannelEngine::ProcessPmcSignal(){
 
+	printf("-----------------------> pmc signal\n");
 	const GRegBits *g_reg = nullptr;
 	GRegBits *g_reg_last = nullptr;
 	FRegBits *f_reg = nullptr;

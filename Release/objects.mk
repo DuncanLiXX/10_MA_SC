bba95f5c469d6dfdb65602752fae6be2829e009e
zoukun <zoukun@maxonic.com.cn>
2022-07-21 09:28:29 +0800

    aaa

--- Release/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -lpthread -lLitronucLicense -Wl,-Map,project.map
-

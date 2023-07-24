cb417c6abb5f617fa4b982bc092d1d7139e4e2eb
lilingxiao <lilingxiao@maxonic.com.cn>
2023-07-24 14:17:19 +0800

    程序再启安全位置使用错误

--- src/channel/channel_control.cpp
+++ src/channel/channel_control.cpp
@@ -3519,7 +3519,7 @@ void ChannelControl::DoRestart(uint64_t line_no){
 
         //Z轴抬到机械原点
         if(phy_axis_z != NO_AXIS){
-            tar.SetAxisValue(chn_axis_z, this->m_p_axis_config[phy_axis_z].axis_home_pos[5]);
+            tar.SetAxisValue(chn_axis_z, this->m_p_axis_config[phy_axis_z].axis_home_pos[4]);
             axis_mask = 0x01<<chn_axis_z;
             msg = new RapidMsg(src, tar, axis_mask);
             if(msg != nullptr){
